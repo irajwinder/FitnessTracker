39c9de8e7c0f68f2ef9855b03675f78c9d1d68d3
Rajwinder Singh <irajwinder@users.noreply.github.com>
2024-02-13 06:16:45 -0500

    Initial Commit

---
+++ FitnessTracker Watch App/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker Watch App
+//
+//  Created by Rajwinder Singh on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTracker Watch AppUITests/FitnessTracker_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FitnessTracker_Watch_AppUITestsLaunchTests.swift
+//  FitnessTracker Watch AppUITests
+//
+//  Created by Rajwinder Singh on 2/13/24.
+//
+
+import XCTest
+
+final class FitnessTracker_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
