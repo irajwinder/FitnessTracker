5ac6b6edb3b0aadf9b270b7444cbec9a1bf64aab
Rajwinder Singh <irajwinder@users.noreply.github.com>
2024-02-13 06:18:38 -0500

    First Commit

--- FitnessTracker Watch App/StartView.swift
+++ FitnessTracker Watch App/StartView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  StartView.swift
 //  FitnessTracker Watch App
 //
 //  Created by Rajwinder Singh on 2/13/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    StartView()
 }
